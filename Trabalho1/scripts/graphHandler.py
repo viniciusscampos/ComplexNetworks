ce997ec16886f78d2a7f636e564c1651b5f3bcfd
Vinicius Campos <silvcamposvinicius@gmail.com>
2017-03-23 20:35:42 -0300

    :art: Convertion to gt format implemented.

---
+++ Trabalho1/scripts/graphHandler.py
@@ -0,0 +1,40 @@
+import json
+import glob
+import graph_tool.all as gt
+
+class graph:
+	def __init__(self):
+		self.configuration = json.loads(open('../config/config.json').read())
+		self.datasets_folder = self.configuration["datasets_folder"]
+		self.datasets = self.configuration["datasets"]
+
+	def file_converter(self):
+		for file_name in self.datasets:
+			file = open("{}{}.txt".format(self.datasets_folder,file_name), "read")		
+			g = Graph()
+			v_id = g.new_vertex_property("int")
+			for line in file:
+				if "# Nodes:" in line:
+					total_nodes = int(line.split(" ")[2])
+					g.add_vertex(total_nodes)
+				if not line.startswith("#"):
+					nodes = line.split("	")
+					v1 = int(nodes[0]) - 1 
+					v2 = int(nodes[1]) - 1
+					g.add_edge(v1,v2)
+			g.save("{}converted/{}.gt".format(self.datasets_folder,file_name))
+
+
+
+
+	def calculate_metrics(self):		
+		graph_files = glob.glob("{}/converted/*.gt".format(self.datasets_folder))
+		for graph_absolute_path in graph_files:
+			graph_name = graph_absolute_path.split("/")[-1]
+			g = gt.load_graph(graph_absolute_path)
+			clust = gt.local_clustering(g)
+			print(gt.vertex_average(g,clust))
+
+
+g = graph()
+g.calculate_metrics()
