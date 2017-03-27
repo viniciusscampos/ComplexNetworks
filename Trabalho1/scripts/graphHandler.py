e8d8e63b160677ec5cbb2c3f4b86d2014a5ef32e
Vinicius Campos <silvcamposvinicius@gmail.com>
2017-03-26 21:45:25 -0300

    :art: Calculate degrees and generate ecdf plot implemented.

--- Trabalho1/scripts/graphHandler.py
+++ Trabalho1/scripts/graphHandler.py
@@ -1,7 +1,11 @@
 import json
 import glob
+from statsmodels.distributions.empirical_distribution import ECDF
+import matplotlib
+matplotlib.use('gtk3agg')
+from matplotlib import pyplot as plt
 from graph_tool.all import *
-from numpy import *
+import numpy as np
 
 class graph:
 	def __init__(self):
@@ -32,17 +36,14 @@ class graph:
 			graph_name = graph_absolute_path.split("/")[-1]
 			g = load_graph(graph_absolute_path)						
 			data = {"name": graph_name, "isDirected": g.is_directed()}
-			# Calculate the strongly connected components
-			#l = gt.label_largest_component(g)
-			#u = gt.GraphView(g,vfilt=l)
 			# Calculate graph degrees metrics
-			data["degrees"] = self.calculate_degrees(g)			
+			data["degrees"] = self.calculate_degrees(g,graph_name.split(".")[0])
 			# Create a file to store the informations						
 			with open('{}results/files/{}-results.json'.format(self.project_folder,graph_name.split(".")[0]),'w') as f:
 				json.dump(str(data),f)
 
 			
-	def calculate_degrees(self,g):
+	def calculate_degrees(self,g,name):
 		"""Receive a graph as a parameter and return the degree metrics for the graph."""
 		if(g.is_directed):
 			outDegrees = g.get_out_degrees(g.get_vertices())
@@ -50,15 +51,42 @@ class graph:
 
 			outDegreesMetrics = self.get_metrics(outDegrees)
 			inDegreesMetrics = self.get_metrics(inDegrees)
+
+			# Generate the ECDF of the out degrees
+			ecdf = ECDF(outDegrees)
+			x = np.linspace(min(outDegrees),max(outDegrees))
+			y = ecdf(x)
+			plt.step(x,y)
+			plt.xlabel("empirical cdf outDegree")
+			plt.savefig('{}results/images/{}-{}-ecdf.png'.format(self.project_folder,name,"outDegrees"))
+
+			# Generate the ECDF of the in degrees
+			ecdf = ECDF(inDegrees)
+			x = np.linspace(min(inDegrees),max(inDegrees))
+			y = ecdf(x)
+			plt.step(x,y)
+			plt.xlabel("empirical cdf inDegree")
+			plt.savefig('{}results/images/{}-{}-ecdf.png'.format(self.project_folder,name,"inDegrees"))
+
 			return {"outDegreesMetrics": outDegreesMetrics, "inDegreesMetrics": inDegreesMetrics}
 		else:
 			outDegrees = g.get_out_degrees(g.get_vertices())
 			outDegreesMetrics = self.get_metrics(outDegrees)
-			return {"outDegreesMetrics": outDegreesMetrics}
 
+			# Generate the ECDF of the out degrees
+			ecdf = ECDF(outDegrees)
+			x = np.linspace(min(outDegrees),max(outDegrees))
+			y = ecdf(x)
+			plt.step(x,y)
+			plt.xlabel("empirical cdf outDegree")
+			plt.savefig('{}results/images/{}-{}-ecdf.png'.format(self.project_folder,name,"outDegrees"))
+
+
+			return {"outDegreesMetrics": outDegreesMetrics}
+		
 	def get_metrics(self,a):
 		"""Receives an array as parameters and return an dictionary containing their min,max,mean and standard deviation metrics. """
-		return {"min": amin(a),"max":amax(a),"mean": mean(a), "standard-deviation": std(a) }
+		return {"min": np.amin(a),"max":np.amax(a),"mean": np.mean(a), "standard-deviation": np.std(a) }
 
 
 g = graph()
